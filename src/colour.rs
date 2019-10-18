dc6ad5ae90d03044b8a86cf8447bec3e2b192992
Russell Mackenzie <russmackdev@gmail.com>
2019-10-18 15:29:55 +0100

    Initial commit.

---
+++ src/colour.rs
@@ -0,0 +1,309 @@
+use std::cmp::Ordering;
+
+/// HSVf uses f64 for all fields.
+/// h is degrees 0.0 to 360.0
+/// s is 0.0 to 1.0
+/// v is 0.0 to 1.0
+#[derive(Debug)]
+pub struct HSVf {
+    pub h: f64,
+    pub s: f64,
+    pub v: f64,
+}
+
+pub struct HSV {
+    pub h: u16,
+    pub s: u8,
+    pub v: u8,
+}
+
+/// RGBf uses f64 for all fields.
+/// There are methods for converting to other representations.
+pub struct RGBf {
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
+}
+
+pub struct RGB {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl HSVf {
+    pub fn to_rgbf(&self) -> Result<RGBf, String> {
+        if self.h < 0.0 || 
+            self.h > 360.0 || 
+                self.s < 0.0 || 
+                self.s > 1.0 || 
+                self.v < 0.0 || 
+                self.v > 1.0 {
+                    let e = format!("error: invalid HSV: {:?}", self);
+                    return Err(e);
+        }
+
+        let h = match self.h {
+            h if h.floor() as i64 == 360   => 0.0,
+            h                              => h / 60.0,
+        };
+
+        let fraction: f64 = h - h.floor();
+
+        let p: f64 = self.v * (1.0 - self.s);
+        let q: f64 = self.v * (1.0 - self.s * fraction);
+        let t: f64 = self.v * (1.0 - self.s * (1.0 - fraction));
+
+        let rgb = match h {
+            _ if 0.0 <= h && h < 1.0    => RGBf { r: self.v,    g: t,       b: p       },
+            _ if 1.0 <= h && h < 2.0    => RGBf { r: q,         g: self.v,  b: p       },
+            _ if 2.0 <= h && h < 3.0    => RGBf { r: p,         g: self.v,  b: t       },
+            _ if 3.0 <= h && h < 4.0    => RGBf { r: p,         g: q,       b: self.v  },
+            _ if 4.0 <= h && h < 5.0    => RGBf { r: t,         g: p,       b: self.v  },
+            _ if 5.0 <= h && h < 6.0    => RGBf { r: self.v,    g: p,       b: q       },
+            _                           => RGBf { r: 0.0,       g: 0.0,     b: 0.0     },
+        };
+
+        Ok(rgb)
+    }
+}
+
+impl RGBf {
+    pub fn to_u8(&self) -> RGB {
+        RGB {
+            r: (self.r * 255.0) as u8,
+            g: (self.g * 255.0) as u8,
+            b: (self.b * 255.0) as u8,
+        }
+    }
+}
+
+impl RGB {
+    pub fn to_hsv(&self) -> HSV {
+        let r1: f32;
+        let g1: f32;
+        let b1: f32;
+
+        let sorted_floats = {
+            r1 = f32::from(self.r) / 255.0;
+            g1 = f32::from(self.g) / 255.0;
+            b1 = f32::from(self.b) / 255.0;
+
+            let mut floats: Vec<f32> = vec![r1, g1, b1];
+            floats.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+
+            floats
+        };
+
+        let cmax = sorted_floats[2];
+        let cmin = sorted_floats[0];
+        let d = cmax - cmin;
+
+        // Hue.
+        let h = match cmax {
+            _ if r1 == cmax => (((g1 - b1) / d) % 6.0) * 60.0,
+            _ if g1 == cmax => (((b1 - r1) / d) + 2.0) * 60.0,
+            _ if b1 == cmax => (((r1 - g1) / d) + 4.0) * 60.0,
+            _ => 0.0,
+        };
+
+        // Saturation.
+        let s = match cmax {
+            _ if cmax == 0.0 => 0.0,
+            _ => d / cmax,
+        };
+
+        // Value / brightness.
+        let v = cmax;
+
+        HSV {
+            h: h as u16,
+            s: (s * 100.0) as u8,
+            v: (v * 100.0) as u8,
+        }
+    }
+}
+ 
+#[cfg(test)]
+mod tests {
+    use crate::colour::{HSV, HSVf, RGB, RGBf};
+
+    #[test]
+    fn test_rgbf_to_u8() {
+        struct Test {
+            rgbf: RGBf, 
+            rgb:  RGB,
+        }
+
+        let tests = vec![
+            Test {
+                rgbf: RGBf { r: 0.0, g: 0.0, b: 0.0, },
+                rgb:  RGB  { r: 0,   g: 0,   b: 0 },
+            },
+            Test {
+                rgbf: RGBf { r: 1.0, g: 1.0, b: 1.0, },
+                rgb:  RGB  { r: 255, g: 255, b: 255 },
+            },
+            Test {
+                rgbf: RGBf { r: 0.5, g: 0.5, b: 0.5, },
+                rgb:  RGB  { r: 127, g: 127, b: 127 },
+            },
+            Test {
+                rgbf: RGBf { r: 1.0,    g: 0.0, b: 0.0, },
+                rgb:  RGB  { r: 255,    g: 0,   b: 0 },
+            },
+        ];
+
+        for (i, j) in tests.iter().enumerate() {
+            let res = j.rgbf.to_u8();
+
+            assert_eq!(
+                j.rgb.r, res.r, "case # {} ; for r expected {}, got {}", 
+                i, j.rgb.r, res.r);
+            assert_eq!(
+                j.rgb.g, res.g, "case # {} ; for g expected {}, got {}", 
+                i, j.rgb.g, res.g);
+            assert_eq!(
+                j.rgb.b, res.b, "case # {} ; for b expected {}, got {}", 
+                i, j.rgb.b, res.b);
+        }
+    }
+
+
+    #[test]
+    fn test_hsv_hsvf_to_rgbf() {
+        struct Test {
+            hsv: HSVf,
+            rgb: RGBf,
+        }
+
+        let tests = vec![
+            Test {
+                hsv: HSVf {
+                    h: 0.0, // degrees as float
+                    s: 0.0, // 0.0 to 1.0
+                    v: 0.0, // 0.0 to 1.0
+                },
+                rgb: RGBf {
+                    r: 0.0,
+                    g: 0.0,
+                    b: 0.0,
+                },
+            },
+            Test {
+                hsv: HSVf {
+                    h: 90.0,
+                    s: 1.0,
+                    v: 1.0,
+                },
+                rgb: RGBf { // chartreuse
+                    r: 0.5, // 127.0,
+                    g: 1.0, // 255.0,
+                    b: 0.0,
+                },
+            },
+            Test {
+                hsv: HSVf {
+                    h: 60.0,
+                    s: 1.0,
+                    v: 0.5,
+                },
+                rgb: RGBf { // olive
+                    r: 0.5, // 128.0,
+                    g: 0.5, // 128.0,
+                    b: 0.0,
+                },
+            },
+        ];
+
+        for (i, t) in tests.iter().enumerate() {
+
+            let hsvf = HSVf { h: t.hsv.h, s: t.hsv.s, v: t.hsv.v };
+            let res = match hsvf.to_rgbf() {
+                Ok(v)   => v,
+                Err(e)  => {
+                    assert!(false, "error converting hsv to rgb: {}", e);
+                    continue;
+                },
+            };
+
+            assert_eq!(
+                t.rgb.r, res.r, "case # {} ; for r expected {}, got {}", 
+                i, t.rgb.r, res.r);
+            assert_eq!(
+                t.rgb.g, res.g, "case # {} ; for g expected {}, got {}", 
+                i, t.rgb.g, res.g);
+            assert_eq!(
+                t.rgb.b, res.b, "case # {} ; for b expected {}, got {}", 
+                i, t.rgb.b, res.b);
+        }
+    }
+
+    #[test]
+    fn test_rgb_to_hsv() {
+        struct Test {
+            rgb: RGB,
+            hsv: HSV,
+        }
+
+        let tests = vec![
+            Test {
+                rgb: RGB {
+                    r: 0,
+                    g: 0,
+                    b: 0,
+                },
+                hsv: HSV {
+                    h: 0,
+                    s: 0,
+                    v: 0,
+                },
+            },
+            Test {
+                rgb: RGB {
+                    r: 255,
+                    g: 255,
+                    b: 255,
+                },
+                hsv: HSV {
+                    h: 0,   // 0
+                    s: 0,   // 0.0
+                    v: 100, // 100.0
+                },
+            },
+            Test {
+                rgb: RGB { // olive
+                    r: 128,
+                    g: 128,
+                    b: 0,
+                },
+                hsv: HSV {
+                    h: 60,
+                    s: 100,
+                    v: 50,  // 50.2
+                },
+            },
+            Test {
+                rgb: RGB { // chartreuse
+                    r: 127,
+                    g: 255,
+                    b: 0,
+                },
+                hsv: HSV {
+                    h: 90,
+                    s: 100,
+                    v: 100,
+                },
+            },
+        ];
+
+        for (i, t) in tests.iter().enumerate() {
+            let res = RGB { r: t.rgb.r, g: t.rgb.g, b: t.rgb.b }.to_hsv();
+
+            assert_eq!(t.hsv.h, res.h, "case # {} ; for r expected {}, got {}", i, t.hsv.h, res.h);
+            assert_eq!(t.hsv.s, res.s, "case # {} ; for g expected {}, got {}", i, t.hsv.s, res.s);
+            assert_eq!(t.hsv.v, res.v, "case # {} ; for b expected {}, got {}", i, t.hsv.v, res.v);
+        }
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,160 @@
+extern crate minifb;
+
+use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
+
+use std::process;
+
+pub mod colour;
+
+use colour::HSVf;
+
+const WIDTH: usize = 720;
+const HEIGHT: usize = 200;
+
+const SAMPLE_WIN_WIDTH: usize = 100;
+const SAMPLE_WIN_HEIGHT: usize = 25;
+
+const SAMPLE_WIN_BUF_SIZE: usize = SAMPLE_WIN_WIDTH * SAMPLE_WIN_HEIGHT;
+
+fn coords_to_hsvf(width: usize, height: usize, x: usize, y: usize, invert_y: bool) -> HSVf {
+    let hue: f64 = (360.0 / width as f64) * x as f64;
+
+    let hsvf: HSVf = if !invert_y {
+        // This is for using coordinates have top-left origin: 0, 0.
+        let sat: f64 = match y {
+            _ if y <= height/2 => y as f64 * (1.0 / (height/2) as f64),
+            _ => 1.0,
+        };
+
+        let val: f64 = match y {
+            _ if y > height/2 => (height - y) as f64 * (1.0 / (height/2) as f64),
+            _ => 1.0,
+
+        };
+        HSVf {h: hue, s: sat, v: val}
+    } else {
+        // This is for using coordinates have bottom-left origin: 0, 0.
+        let sat: f64 = match y {
+            _ if y >= height/2 => (height-y) as f64 * (1.0 / (height/2) as f64),
+            _ => 1.0,
+        };
+
+        let val: f64 = match y {
+            _ if y < height/2 => y as f64 * (1.0 / (height/2) as f64),
+            _ => 1.0,
+        };
+        HSVf {h: hue, s: sat, v: val}
+    };
+
+    hsvf
+}
+
+fn main() {
+    // Main, big window for colour palette.
+    let mut window = match Window::new("Weekend Raytracer 01", WIDTH, HEIGHT, 
+        WindowOptions {
+            ..WindowOptions::default()
+        }) {
+        Ok(win)     => win,
+        Err(err)    => {
+            println!("Unable to create main window {}", err);
+            return;
+        }
+    };
+
+    let mut buffer: Vec<u32> = Vec::with_capacity(WIDTH * HEIGHT);
+
+    // Small window for colour sample.
+    let mut sample_win = match Window::new("", SAMPLE_WIN_WIDTH, SAMPLE_WIN_HEIGHT,
+        WindowOptions {
+            ..WindowOptions::default()
+        }) {
+        Ok(win)     => win,
+        Err(err)    => {
+            println!("Unable to create colour sample window {}", err);
+            return;
+        }
+    };
+
+    let mut sample_win_buf: [u32; SAMPLE_WIN_BUF_SIZE] = [0; SAMPLE_WIN_BUF_SIZE];
+
+
+    let mut size = (0, 0);
+
+    let nx = WIDTH;
+    let ny = HEIGHT;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        {
+            let new_size = window.get_size();
+            if new_size != size {
+                size = new_size;
+                buffer.resize(size.0 * size.1, 0);
+            }
+        }
+
+        let mut i = 0;
+        for y in (0..ny).rev() {
+            for x in 0..nx {
+                // Use the same coordinate system as the mouse - top-left: 0, 0.
+                let coord_y = ny - y;
+                let hsvf = coords_to_hsvf(nx, ny, x as usize, coord_y as usize, false);
+
+                let rgbf = match hsvf.to_rgbf() {
+                    Ok(v)   => v,
+                    Err(e)  => {
+                        println!("error converting hsvf to rgbf: {}", e);
+                        process::exit(1);
+                    },
+                };
+
+                let ir = (255.99 * rgbf.r as f32).floor() as u32 * 65536;
+                let ig = (255.99 * rgbf.g as f32).floor() as u32 * 256;
+                let ib = (255.99 * rgbf.b as f32).floor() as u32;
+
+                buffer[i] = ir + ig + ib;
+
+                i += 1;
+            }
+        }
+
+        if let Some((x, y)) = window.get_mouse_pos(MouseMode::Discard) {
+            let hsvf = coords_to_hsvf(nx, ny, x as usize, y as usize, false);
+
+            let rgbf = match hsvf.to_rgbf() {
+                Ok(v)   => v,
+                Err(e)  => {
+                    println!("error converting hsvf to rgbf: {}", e);
+                    process::exit(1);
+                },
+            };
+
+            let ir = (255.99 * rgbf.r as f32).floor() as u32 * 65536;
+            let ig = (255.99 * rgbf.g as f32).floor() as u32 * 256;
+            let ib = (255.99 * rgbf.b as f32).floor() as u32;
+
+            let rgb = rgbf.to_u8();
+
+            let win_title = format!( "[ x: {}, y: {} ]  r: {}, g: {}, b: {}",
+                x.floor(), y.floor(), rgb.r, rgb.g, rgb.b);
+
+            window.set_title(&win_title);
+            
+            if window.get_mouse_down(MouseButton::Left) {
+                println!("{}", win_title);
+            }
+
+            // Update the sample window buffer.
+            let rgb_colour = ir + ig + ib;
+            
+            for f in sample_win_buf.iter_mut() {
+                *f = rgb_colour;
+            }
+        };
+
+        window.update_with_buffer(&buffer).unwrap();
+
+        sample_win.update_with_buffer(&sample_win_buf).unwrap();
+    }
+}
+
